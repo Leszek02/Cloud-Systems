58e168fa7e936722e1dd9cb49c7dbd4b86dd1b8d
Leszek02 <leszek.miernicki02@gmail.com>
2026-03-14 20:54:22 +0100

    (stage 5) Add cluster & AR

--- stage_5/clusters.tf
+++ stage_5/clusters.tf
@@ -1,27 +1,46 @@
-# resource "google_service_account" "k8s_service_account" {
-#   account_id   = "service-account-id"
-#   display_name = "A service account to manage applications within the k8s cluster"
-# }
+resource "google_service_account" "k8s_service_account" {
+  project      = var.project_id
+  account_id   = "node-pool-sa-21371"
+  display_name = "A service account to manage applications within the k8s cluster"
+}
 
-# resource "google_container_cluster" "primary" {
-#   name     = "cloud-systems-cluster"
-#   location = "eu-east-5"
+resource "google_container_cluster" "cloud-systems-cluster" {
+  name     = "cloud-systems-cluster"
+  project  = var.project_id
+  location = local.location
 
-#   remove_default_node_pool = true
-#   initial_node_count       = 0
-# }
+  remove_default_node_pool = true
+  initial_node_count       = 1
+  deletion_protection = false
 
-# resource "google_container_node_pool" "primary_preemptible_nodes" {
-#   name       = "my-node-pool"
-#   location   = "eu-east-5"
-#   cluster    = google_container_cluster.primary.name
-#   node_count = 1
+  node_config {
+    boot_disk { 
+      size_gb = 20
+    }
+    spot = true
+  }
+    cluster_autoscaling {
+      auto_provisioning_defaults {
+        disk_size = 20
+      }
+    }
+}
 
+resource "google_container_node_pool" "cloud-systems-node-pool" {
+  name     = "cloud-systems-node-pool"
+  project  = var.project_id
+  location = local.location
+  cluster  = google_container_cluster.cloud-systems-cluster.id
 
-#   node_config {
-#     preemptible  = true
-#     machine_type = "n1-standard-2"
-#     spot       = true
-#     service_account = google_service_account.default.email
-#   }
-# }
+  node_config {
+    machine_type    = "n1-standard-2"
+    spot            = true
+    service_account = google_service_account.k8s_service_account.email
+    disk_size_gb = 20
+  }
+  autoscaling {
+    min_node_count = 0
+    max_node_count = 2 
+  }
+}
+
--- stage_5/variables.tf
+++ stage_5/variables.tf
@@ -1,9 +1,9 @@
 variable "project_id" {
-    description = "ID of a gcp project that owns declared resources"
-    type = string
+  description = "ID of a gcp project that owns declared resources"
+  type        = string
 }
 
 variable "owner-email" {
-    description = "Email of a project co-owner"
-    type = string
+  description = "Email of a project co-owner"
+  type        = string
 }
