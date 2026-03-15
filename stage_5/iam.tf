9219a7285186ef1be47ca8ef08116289f102d5be
Leszek02 <leszek.miernicki02@gmail.com>
2026-03-15 16:01:07 +0100

    (stage 5) Remove unused variable

--- stage_5/iam.tf
+++ stage_5/iam.tf
@@ -1,9 +1,1 @@
 
-resource "google_project_iam_binding" "project" {
-  project = var.project_id
-  role    = "roles/admin"
-
-  members = [
-    var.owner-email,
-  ]
-}
--- stage_5/variables.tf
+++ stage_5/variables.tf
@@ -2,8 +2,3 @@ variable "project_id" {
   description = "ID of a gcp project that owns declared resources"
   type        = string
 }
-
-variable "owner-email" {
-  description = "Email of a project co-owner"
-  type        = string
-}
